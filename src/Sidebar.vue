cd256ad73bf1e4e1653658f434970f7c4d083031
Nick Renteria <Nick Renteria>
2020-04-12 16:04:40 -0400

    began media queries

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -16,25 +16,25 @@
         <ul class="inbox-nav">
             <li :class="{ active : activeView == 'app-inbox' }">
                 <a href="#" @click.prevent="navigate('app-inbox', 'Inbox')">
-                    <i class="fa fa-inbox"></i>Inbox <span class="label label-danger pull-right">{{ unreadMessages.length }}</span>
+                    <i class="fa fa-inbox"></i><span class="inbox-nav_text">Inbox </span><span class="label label-danger pull-right">{{ unreadMessages.length }}</span>
                 </a>
             </li>
 
             <li :class="{ active : activeView == 'app-sent' }">
                 <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
-                    <i class="fa fa-envelope-o"></i>Sent <span class="label label-default pull-right">{{ sentMessages.length }}</span>
+                    <i class="fa fa-envelope-o"></i><span class="inbox-nav_text">Sent </span><span class="label label-default pull-right">{{ sentMessages.length }}</span>
                 </a>
             </li>
 
             <li :class="{ active : activeView == 'app-important' }">
                 <a href="#" @click.prevent="navigate('app-important', 'Important')" >
-                    <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">{{ importantMessages.length }}</span>
+                    <i class="fa fa-bookmark-o"></i><span class="inbox-nav_text">Important </span><span class="label label-warning pull-right">{{ importantMessages.length }}</span>
                 </a>
             </li>
 
             <li :class="{ active : activeView == 'app-trash' }">
                 <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
-                    <i class=" fa fa-trash-o"></i>Trash <span class="label label-default pull-right">{{ trashedMessages.length }}</span>
+                    <i class=" fa fa-trash-o"></i><span class="inbox-nav_text">Trash </span><span class="label label-default pull-right">{{ trashedMessages.length }}</span>
                 </a>
             </li>
         </ul>
