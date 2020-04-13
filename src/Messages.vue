c05f63771d2f7e8f771b459478366c709cdfab28
Nick Renteria <Nick Renteria>
2020-04-13 17:10:03 -0400

    enabled checkbox click

--- src/Messages.vue
+++ src/Messages.vue
@@ -1,17 +1,17 @@
 <template>
     <table v-if="messages.length > 0" class="table table-inbox table-hover">
         <tbody>
-            <tr v-for="message in messages" @click="openMessage(message)" :class="{ unread: typeof message.isRead !== 'undefined' && !message.isRead }">
+            <tr v-for="message in messages" :class="{ unread: typeof message.isRead !== 'undefined' && !message.isRead }">
                 <td><input type="checkbox"></td>
                 <td>
                     <a href="#" v-if="typeof message.isImportant !== 'undefined'" @click.prevent.stop="message.isImportant = !message.isImportant">
                         <i :class="['fa', 'fa-star', { 'important' : message.isImportant }]"></i>
                     </a>
                 </td>
-                <td>{{ message.from.name }}</td>
-                <td>{{ message.subject }}</td>
-                <td><i v-if="message.attachments.length > 0" class="fa fa-paperclip"></i></td>
-                <td class="text-right">{{ message.date.fromNow() }}</td>
+                <td @click="openMessage(message)">{{ message.from.name }}</td>
+                <td @click="openMessage(message)">{{ message.subject }}</td>
+                <td @click="openMessage(message)"><i v-if="message.attachments.length > 0" class="fa fa-paperclip"></i></td>
+                <td class="text-right" @click="openMessage(message)">{{ message.date.fromNow() }}</td>
             </tr>
         </tbody>
     </table>
